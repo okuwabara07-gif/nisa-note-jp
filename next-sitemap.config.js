7887db64697137281041627fd5428d836f7e84f3
Osamu Kuwabara <o.kuwabara07@gmail.com>
2026-05-05 07:47:02 +0900

    fix: next-sitemap の自動 robots.txt 生成を無効化（手動noindex化対応）

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://nisa-note-jp.vercel.app',
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
   robotsTxtOptions: {
     additionalSitemaps: ['https://nisa-note-jp.vercel.app/sitemap.xml'],
   },
